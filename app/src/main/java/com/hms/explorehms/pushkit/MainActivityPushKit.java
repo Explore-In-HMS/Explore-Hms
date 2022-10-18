59e55732f1a1353b9b4aa8e28fa6b2807d9b3484
erdalkaymak <erdalkaymak34@gmail.com>
2022-10-18 18:16:21 +0300

    Fix Push kit subscribe and unsubscribe first time fail

--- app/src/main/java/com/hms/explorehms/pushkit/MainActivityPushKit.java
+++ app/src/main/java/com/hms/explorehms/pushkit/MainActivityPushKit.java
@@ -28,8 +28,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -37,19 +36,20 @@ import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 
+import com.github.clemp6r.futuroid.Async;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.hms.explorehms.R;
 import com.hms.explorehms.Util;
 import com.hms.explorehms.pushkit.dialog.GetNotificationDialog;
 import com.hms.explorehms.pushkit.dialog.PushMessageDialogPushKit;
-import com.hms.explorehms.pushkit.dialog.TopicDialogPushKit;
-import com.github.clemp6r.futuroid.Async;
 import com.huawei.agconnect.AGConnectOptions;
 import com.huawei.agconnect.AGConnectOptionsBuilder;
-import com.huawei.agconnect.config.AGConnectServicesConfig;
 import com.huawei.hmf.tasks.Task;
 import com.huawei.hms.aaid.HmsInstanceId;
 import com.huawei.hms.aaid.entity.AAIDResult;
@@ -78,6 +78,7 @@ public class MainActivityPushKit extends AppCompatActivity {
     private Button btnNext;
     private Button btnGetNotification;
     private ViewFlipper viewFlipper;
+    private AlertDialog dialog;
 
     /**
      * AAID related views
@@ -108,11 +109,12 @@ public class MainActivityPushKit extends AppCompatActivity {
     private TextView tvSubUnsubResult;
     private TextView tvAutoInitOnOffResult;
     private TextView tvSubTopic;
+    private HmsMessaging messaging;
 
     /**
      * Push Message related variables
      */
-    private static final String EXPLOREHMS_ACTION = "com.hms.explorehms.huawei.feature_pushkit.action";
+    private static final String EXPLOREHMS_ACTION = "com.hms.explorehms.feature_pushkit.action";
     private PushMessageDialogPushKit pushMessageDialog;
     private GetNotificationDialog getNotificationDialog;
 
@@ -121,9 +123,8 @@ public class MainActivityPushKit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_pushkit);
+        messaging=HmsMessaging.getInstance(MainActivityPushKit.this);
         setupToolbar();
-
-
         initUI();
         initListener();
         initReceiver();
@@ -300,12 +301,15 @@ public class MainActivityPushKit extends AppCompatActivity {
             new Thread(() -> {
                 try {
                     HmsInstanceId.getInstance(MainActivityPushKit.this).deleteAAID();
-                    tvAAIDResult.setText("");
-                    tvAAIDOprResult.setText(getResources().getString(R.string.txt_opr_res_success_pushkit));
+                    runOnUiThread(() -> {
+                        tvAAIDResult.setText("");
+                        tvAAIDOprResult.setText(getResources().getString(R.string.txt_opr_res_success_pushkit));
+                    });
                 } catch (Exception e) {
                     Log.e(TAG, "deleteAAID failed. " + e);
-                    tvAAIDOprResult.setText(getResources().getString(R.string.txt_opr_res_failed_pushkit));
+                    runOnUiThread(() -> tvAAIDOprResult.setText(getResources().getString(R.string.txt_opr_res_failed_pushkit)));
                 }
+                ;
             }).start();
         }
     }
@@ -348,7 +352,7 @@ public class MainActivityPushKit extends AppCompatActivity {
                 Log.i(TAG, "get token:" + token);
 
                 // Check whether the token is empty.
-                if(!TextUtils.isEmpty(token)) {
+                if (!TextUtils.isEmpty(token)) {
                     sendRegTokenToServer(token);
                 }
             } catch (ApiException e) {
@@ -440,66 +444,48 @@ public class MainActivityPushKit extends AppCompatActivity {
      * to subscribe to topics in asynchronous mode.
      */
     private void addTopic() {
+        createDialog(getResources().getString(R.string.txt_sub_name_pushkit), true);
+    }
 
-        TopicDialogPushKit dialog = new TopicDialogPushKit(MainActivityPushKit.this, true, topic -> {
+    private void createDialog(String text, boolean isAdd) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_add_topic_pushkit, null);
+        TextInputEditText edTopic;
+        Button btnCancel;
+        Button btnConfirm;
+        btnConfirm = view.findViewById(R.id.btn_push_topic_confirm);
+        btnCancel = view.findViewById(R.id.btn_push_topic_cancel);
+        edTopic = view.findViewById(R.id.ed_push_topic);
+        TextInputLayout edLayout = view.findViewById(R.id.et_Layout);
+        edLayout.setHint(isAdd ? "Subscribe topic name" : "Unsubscribe topic name");
+        btnConfirm.setOnClickListener(v -> {
             try {
-                HmsMessaging.getInstance(MainActivityPushKit.this)
-                        .subscribe(topic)
+                messaging.subscribe(Objects.requireNonNull(edTopic.getText()).toString())
                         .addOnCompleteListener(task -> {
                             if (task.isSuccessful()) {
                                 tvSubUnsubResult.setText(getResources().getString(R.string.txt_success_pushkit));
-                                //Txt will be displayed -> (Subscribed : topic);
-                                tvSubTopic.setText(String.format(getResources().getString(R.string.txt_sub_name_pushkit), topic));
+                                tvSubTopic.setText(String.format(text, Objects.requireNonNull(edTopic.getText())));
                             } else {
                                 tvSubUnsubResult.setText(getResources().getString(R.string.txt_failed_pushkit));
                             }
                         });
             } catch (Exception e) {
-                Log.i(TAG, "subscribe Failed" + e.toString());
+                Log.i(TAG, "subscribe Failed" + e);
                 Toast.makeText(getApplicationContext(), getResources().getString(R.string.msg_topic_sub_exception), Toast.LENGTH_LONG).show();
             }
+            dialog.dismiss();
         });
-        try {
-            WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-            lp.copyFrom(Objects.requireNonNull(dialog.getWindow()).getAttributes());
-            lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
-            lp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-            dialog.getWindow().setAttributes(lp);
-            dialog.show();
-        } catch (Exception e) {
-            Log.e(TAG, e.toString());
-        }
+        btnCancel.setOnClickListener(v -> dialog.dismiss());
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.show();
     }
 
     /**
      * to subscribe to topics in asynchronous mode.
      */
     private void deleteTopic() {
-        TopicDialogPushKit dialog = new TopicDialogPushKit(MainActivityPushKit.this, false, topic -> {
-            try {
-                HmsMessaging.getInstance(MainActivityPushKit.this)
-                        .unsubscribe(topic)
-                        .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                tvSubUnsubResult.setText(getResources().getString(R.string.txt_success_pushkit));
-                                //Txt will be displayed -> (Unsubscribed : topic);
-                                tvSubTopic.setText(String.format(getResources().getString(R.string.txt_unsub_name_pushkit), topic));
-
-                            } else {
-                                tvSubUnsubResult.setText(getResources().getString(R.string.txt_failed_pushkit));
-                            }
-                        });
-            } catch (Exception e) {
-                Log.i(TAG, "unsubscribe Failed" + e.toString());
-            }
-        });
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-        lp.copyFrom(Objects.requireNonNull(dialog.getWindow()).getAttributes());
-        lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
-        lp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-        dialog.getWindow().setAttributes(lp);
-        dialog.show();
-
+        createDialog(getResources().getString(R.string.txt_unsub_name_pushkit), false);
     }
 
     /**
